db01365eb01ec2aaef405c4ee43dab67ff1e1413
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-05-10 05:16:55 +0700

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -2,10 +2,10 @@
 
 
 
-$cookie ='xxxxxxx';
+$cookie ='bitmedia_fid=eyJmaWQiOiJmOTJmZTExYTU1Y2VlMzUwMWNhYjVkODc2MTcxMmU3OCIsImZpZG5vdWEiOiI1ZWMzMTgzY2M5NmIwODViOTUxODc1Y2NkODYzNjMzMCJ9; captcha=recaptchav2; captcha=recaptchav2; ci_session=6sv4pb0baq2fmoq9os11cl8oli73b478';
 
 
 
 
 
-$user ='xxxxx';
+$user ='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36';
